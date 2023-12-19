6086562fc9a20bc7b88971632fe6a3493f74ba86
정광철 <jung.kc@huray.net>
2023-12-19 22:13:40 +0900

    [update] kafka producer config -> yml

--- src/main/java/com/example/praticekafka/producer/service/ProducerService.java
+++ src/main/java/com/example/praticekafka/producer/service/ProducerService.java
@@ -1,6 +1,7 @@
 package com.example.praticekafka.producer.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -8,10 +9,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProducerService {
 
-    private static final String TOPIC_NAME = "topic1";
+    @Value("${kafka.hello.topic}")
+    private String topic;
+
+    @Value("${kafka.hello.key}")
+    private String helloKey;
+
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void send(String message){
-        kafkaTemplate.send(TOPIC_NAME, message);
+        kafkaTemplate.send(topic, helloKey, message);
     }
 }
